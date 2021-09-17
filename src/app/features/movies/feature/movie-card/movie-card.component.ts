705fbbb1c7d1bb343282fb7f6ce6be0729f4c8e1
Aleix Suau <info@macrofonoestudio.es>
2021-09-17 09:56:57 +0200

    Movie Cards + Material

---
+++ src/app/features/movies/feature/movie-card/movie-card.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/features/movies/data-access/movies/movies.service';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss']
+})
+export class MovieCardComponent implements OnInit {
+  @Input() movie: IMovie;
+  
+  movieDetail: IMovieDetail;
+  detailVisible: boolean;
+  movieUrlBase = `https://www.imdb.com/title/`;
+
+  constructor(
+    private moviesService: MoviesService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  showDetail(id: string): void {
+    if (!this.movieDetail) {
+      this.moviesService.getMovie(id).subscribe((movieDetail) => {
+        this.movieDetail = movieDetail;
+        this.detailVisible = true;
+      });
+    } else {
+      this.detailVisible = true;
+    }
+  }
+}
